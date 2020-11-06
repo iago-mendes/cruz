60ac48b486b1ebea12fcf47cf1b7306410c75613
Iago <iagobrazmendes@gmail.com>
2020-11-06 15:24:00 -0300

    add user context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import Sidebar from '../components/sidebar'
 import '../styles/global.css'
 
+import {User, UserInterface, defaultUser} from '../utils/userContext'
+
 function MyApp({ Component, pageProps }) {
+  const savedUser = localStorage.getItem('@cruz-representacoes/user')
+
+  let user = defaultUser
+  if (savedUser)
+  {
+    const tmp: UserInterface = JSON.parse(savedUser)
+    user.token = tmp.token
+    user.id = tmp.id
+    user.role = tmp.role
+  }
+
   return (
-    <>
+    <User.Provider value={user}>
       <Sidebar/>
       <Component {...pageProps} />
-    </>
+    </User.Provider>
   )
 }
 
---
+++ src/utils/userContext.ts
@@ -0,0 +1,33 @@
+import {createContext} from 'react'
+
+export interface UserInterface
+{
+    token: string
+    id: string
+    role: string
+}
+
+class defaultUserClass
+{
+    token: string
+    id: string
+    role: string
+    setUser: Function
+
+    constructor()
+    {
+        this.token = ''
+        this.id = ''
+        this.role = ''
+        this.setUser = (user: UserInterface) =>
+        {
+            this.token = user.token
+            this.id = user.id
+            this.role = user.role
+        }
+    }
+}
+
+export const defaultUser: UserInterface & {setUser: Function} = new defaultUserClass()
+
+export const User = createContext(defaultUser)
