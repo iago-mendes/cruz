c2076e32081b22dab6d19c30cf790f9a09f2abb2
Iago <iagobrazmendes@gmail.com>
2020-11-06 10:32:40 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Cruz Representações</title>
+      </Head>
+      <h1>Hello world!</h1>
+    </div>
+  )
+}
