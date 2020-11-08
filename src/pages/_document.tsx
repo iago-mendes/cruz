b463f62dae878c83534fd34d3ef2c1458371f2b7
Iago <iagobrazmendes@gmail.com>
2020-11-08 19:03:53 -0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <Html lang="pt">
         <Head>
             <meta charSet="utf-8" />
+            <link rel="icon" href="/favicon.svg" />
             <link
                 href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu:wght@700&display=swap"
                 rel="stylesheet"
