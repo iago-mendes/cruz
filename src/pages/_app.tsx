687fe4d28031237c8c78fa7fdf07a0b4ca2854d1
Iago <iagobrazmendes@gmail.com>
2020-11-26 11:43:27 -0300

    add styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-// import ContextProvider from '../components/ContextProvider'
 import Sidebar from '../components/Sidebar'
 import {Provider} from 'next-auth/client'
+import {ThemeProvider} from 'styled-components'
+
+import theme from '../styles/theme'
 
 import '../styles/global.css'
 import '../styles/pages/login.css'
@@ -11,8 +13,10 @@ import '../styles/pages/empresas/[company]/editar.css'
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
-      <Sidebar/>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+				<Sidebar/>
+				<Component {...pageProps} />
+			</ThemeProvider>
     </Provider>
   )
 }
