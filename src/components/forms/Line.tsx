05775da21de08da04b064e57e83c8cfb6e9e553f
Iago <iagobrazmendes@gmail.com>
2020-12-03 11:29:14 -0300

    create line form

---
+++ src/components/forms/Line.tsx
@@ -0,0 +1,102 @@
+import {useRouter} from 'next/router'
+import {FormEvent, useEffect, useState} from 'react'
+
+import Container from '../../styles/components/forms/Line'
+import api from '../../services/api'
+
+export interface Line
+{
+	id: string
+	imagem: string
+	nome: string
+}
+
+interface LineFormProps
+{
+	method: string
+	
+	nome: string
+	setNome: Function
+	
+	id?: string
+	line?: Line
+}
+
+const LineForm: React.FC<LineFormProps> = ({method, nome, setNome, id, line}) =>
+{
+	const Router = useRouter()
+
+	const [imagem, setImagem] = useState<File>()
+
+	useEffect(() =>
+	{
+		if (line)
+		{
+			setNome(line.nome)
+		}
+	}, [line])
+
+	async function handleSubmit(e: FormEvent)
+	{
+		e.preventDefault()
+
+		const data = new FormData()
+
+		if (imagem) data.append('imagem', imagem)
+		data.append('nome', nome)
+
+		if (method === 'post')
+		{
+			await api.post('companies', data)
+			.then(() =>
+			{
+				alert('Empresa criada com sucesso!')
+				Router.back()
+			})
+			.catch(err =>
+			{
+				console.error(err)
+				alert('Algo errado aconteceu!')
+			})
+		}
+		else if (method === 'put')
+		{
+			await api.put(`companies/${id}`, data)
+			.then(() =>
+			{
+				alert('Empresa atualizada com sucesso!')
+				Router.back()
+			})
+			.catch(err =>
+			{
+				console.error(err)
+				alert('Algo errado aconteceu!')
+			})
+		}
+	}
+
+	return (
+		<Container onSubmit={handleSubmit} >
+			<div>
+				<label htmlFor="imagem">Imagem</label>
+				<input type="file" name="imagem" id="imagem" onChange={e => setImagem(e.target.files[0])}/>
+			</div>
+			<div>
+				<label htmlFor="nome">Nome</label>
+				<input
+					type="text"
+					name="nome"
+					id="nome"
+					value={nome}
+					onChange={e => setNome(e.target.value)}
+				/>
+			</div>
+			<div className="buttons">
+				<button type="button" onClick={Router.back}>Cancelar</button>
+				<button type="submit">Confirmar</button>
+			</div>
+		</Container>
+	)
+}
+
+export default LineForm
