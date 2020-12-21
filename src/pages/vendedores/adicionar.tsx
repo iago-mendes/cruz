bc2d9e08efc90a6de3810a9567d5b5b22aa8b0fd
Iago <iagobrazmendes@gmail.com>
2020-12-21 09:49:52 -0300

    complete add seller page

--- src/pages/vendedores/adicionar.tsx
+++ src/pages/vendedores/adicionar.tsx
@@ -1,18 +1,15 @@
 import {useState} from 'react'
 import Head from 'next/head'
 import {useSession} from 'next-auth/client'
-import {useRouter} from 'next/router'
 
-import Container from '../../styles/pages/vendedores/adicionar'
 import SellerForm from '../../components/forms/Seller'
 import Loading from '../../components/Loading'
 import User from '../../utils/userType'
 import NotAllowed from '../../components/NotAllowed'
+import Header from '../../components/Header'
 
 const AddCompany: React.FC = () =>
 {
-	const Router = useRouter()
-
 	const [session, loading] = useSession()
 	const [nome, setNome] = useState('')
 	
@@ -25,19 +22,21 @@ const AddCompany: React.FC = () =>
 		return <NotAllowed />
 
 	return (
-		<Container className="container">
+		<div className='container'>
 			<Head>
 				<title>{nome} | Cruz Representações</title>
 			</Head>
 
-			{/* <main> */}
+			<Header display={nome} />
+
+			<main className='main'>
 				<SellerForm
 					method='post'
 					nome={nome}
 					setNome={setNome}
 				/>
-			{/* </main> */}
-		</Container>
+			</main>
+		</div>
 	)
 }
 
