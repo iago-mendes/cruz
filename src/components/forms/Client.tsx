5a62b4b375ed18549563f49ee4e1e6688a4a7ae8
Iago <iagobrazmendes@gmail.com>
2020-12-27 19:11:01 -0300

    get static info in clients page

---
+++ src/components/forms/Client.tsx
@@ -0,0 +1,35 @@
+interface ClientCompany
+{
+	_id: string
+	id: string
+	tabela: string
+}
+
+export interface Client
+{
+	_id: string
+	razao_social: string
+	nome_fantasia: string
+	imagem: string
+	cnpj: string
+	insc_estadual: string
+	email: string
+	senha: string
+	vendedores: string[]
+	representadas: ClientCompany[]
+	endereco:
+	{
+		rua: string
+		numero: number
+		bairro: string
+		cep: number
+		cidade: string
+		uf: string
+	}
+	status:
+	{
+		ativo: boolean
+		aberto: boolean
+		nome_sujo: boolean
+	}
+}
--- src/pages/clientes/index.tsx
+++ src/pages/clientes/index.tsx
@@ -1,9 +1,21 @@
+import {GetStaticProps} from 'next'
 import Head from 'next/head'
+import {useEffect} from 'react'
+
+import {Client} from '../../components/forms/Client'
+import api from '../../services/api'
 
 import Container from '../../styles/pages/clientes/index'
 
-const Clients: React.FC = () =>
+interface ClientsProps
 {
+	clients: Client[]
+}
+
+const Clients: React.FC<ClientsProps> = ({clients: staticClients}) =>
+{
+	useEffect(() => console.log('[staticClients]', staticClients), [staticClients])
+
 	return (
 		<Container>
 			<Head>
@@ -11,6 +23,18 @@ const Clients: React.FC = () =>
 			</Head>
 		</Container>
 	)
+}
+
+export const getStaticProps: GetStaticProps = async ctx =>
+{
+	let clients: Client[] = []
+
+	await api.get('clients-raw').then(({data}) => clients = data)
+
+	return {
+		props: {clients},
+		revalidate: 1
+	}
 }
 
 export default Clients
