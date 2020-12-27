e7c605c906d5d8a087861c872f4d6d719f5d5a96
Iago <iagobrazmendes@gmail.com>
2020-12-27 19:02:20 -0300

    create clients page

---
+++ src/pages/clientes/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+import Container from '../../styles/pages/clientes/index'
+
+const Clients: React.FC = () =>
+{
+	return (
+		<Container>
+			<Head>
+				<title>Clientes | Cruz Representações</title>
+			</Head>
+		</Container>
+	)
+}
+
+export default Clients
