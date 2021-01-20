8b9074f939203ebbcd9e1183e939803cbcfedfab
Iago <iagobrazmendes@gmail.com>
2021-01-20 11:24:02 -0300

    style request product modal

--- src/styles/components/forms/RequestProductModal.ts
+++ src/styles/components/forms/RequestProductModal.ts
@@ -1,7 +1,52 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
-	
+	width: 75vw;
+	height: 75vh;
+
+	background-color: ${p => p.theme.colors.background};
+	border-radius: 1rem;
+
+	header
+	{
+		background-color: ${p => p.theme.colors.primary};
+		padding: 0.5rem;
+		border-top-left-radius: 1rem;
+		border-top-right-radius: 1rem;
+
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+
+		button
+		{
+			background: none;
+			border: none;
+			border-radius: 100rem;
+
+			width: 3rem;
+			height: 3rem;
+			color: ${p => p.theme.colors.secondary};
+
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			transition: 0.25s;
+			cursor: pointer;
+
+			:hover
+			{
+				background-color: ${p => p.theme.colors.secondary};
+				color: ${p => p.theme.colors.primary};
+			}
+		}
+	}
+
+	form
+	{
+		padding: 1rem;
+	}
 `
 
 export default Container
