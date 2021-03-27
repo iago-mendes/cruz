4ad3d6bbd2974517b467b56045652099c5d8f2f2
Iago <iagobrazmendes@gmail.com>
2021-03-27 17:27:50 -0300

    create warning alert

---
+++ src/utils/alerts/warning.ts
@@ -0,0 +1,14 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
+
+function warningAlert(message: string)
+{
+	MySwal.fire({
+		icon: 'warning',
+		title: message
+	})
+}
+
+export default warningAlert
