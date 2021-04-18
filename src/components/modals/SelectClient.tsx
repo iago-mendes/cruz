e8fb7b642dd2a15c303b9a57613f344937d441ee
Iago <iagobrazmendes@gmail.com>
2021-04-18 17:52:09 -0300

    create select client modal

---
+++ src/components/modals/SelectClient.tsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from 'react'
+import { FiSearch, FiX } from 'react-icons/fi'
+import { ClientListed } from '../../models/client'
+import api from '../../services/api'
+import Container from '../../styles/components/modals/SelectClient'
+import Paginate from '../Paginate'
+import ModalContainer from './Container'
+
+interface SelectClientModalProps
+{
+	isOpen: boolean
+	setIsOpen: (p: boolean) => void
+}
+
+const SelectClientModal: React.FC<SelectClientModalProps> = ({isOpen, setIsOpen}) =>
+{
+	const [search, setSearch] = useState('')
+	const [clients, setClients] = useState<ClientListed[]>([])
+	const [page, setPage]	= useState(1)
+	const [totalPages, setTotalPages] = useState(1)
+	const [loading, setLoading] = useState(false)
+
+	useEffect(() =>
+	{
+		setLoading(true)
+		updateClients()
+	}, [search])
+
+	async function updateClients()
+	{
+		await api.get('clients', {params: {page, search}})
+			.then(({data, headers}:{data: ClientListed[], headers: any}) =>
+			{
+				setClients(data)
+
+				const tmpPage = Number(headers['page'])
+				if (Number.isNaN(tmpPage))
+					setPage(1)
+				else
+					setPage(tmpPage)
+					
+				const tmpTotalPages = Number(headers['total-pages'])
+				if (Number.isNaN(tmpTotalPages))
+					setTotalPages(1)
+				else
+					setTotalPages(tmpTotalPages)
+			})
+			.catch(error =>
+			{
+				console.log('[error]', error)
+				setClients([])
+
+				setPage(1)
+				setTotalPages(1)
+			})
+		
+		setLoading(false)
+	}
+
+	return (
+		<ModalContainer
+			isOpen={isOpen}
+			setIsOpen={setIsOpen}
+		>
+			<Container>
+				<div className='search'>
+					<FiSearch />
+					<input
+						type='text'
+						value={search}
+						onChange={e => setSearch(e.target.value)}
+						placeholder='Pesquise por um cliente'
+					/>
+					<button className='clear'>
+						<FiX />
+					</button>
+				</div>
+				<Paginate
+					page={page}
+					setPage={setPage}
+					totalPages={totalPages}
+					loading={loading}
+					noResults={clients.length === 0 && !loading}
+				>
+					{clients.map((client, index) => (
+						<div className='client' key={index} >
+							<div className='img'>
+								<img src={client.imagem} alt={client.nome_fantasia} />
+							</div>
+							<div className='info'>
+								<span className='highlight'>
+									{client.nome_fantasia}
+								</span>
+								<span>
+									{client.razao_social}
+								</span>
+							</div>
+						</div>
+					))}
+				</Paginate>
+			</Container>
+		</ModalContainer>
+	)
+}
+
+export default SelectClientModal
