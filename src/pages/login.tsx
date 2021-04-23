38aaeffb764a2ba1aecfe10adff3fc273d600603
Iago <iagobrazmendes@gmail.com>
2021-04-23 16:57:51 -0300

    improve login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,50 @@
 import Head from 'next/head'
-import {ChangeEvent, FormEvent, useState} from 'react'
+import {ChangeEvent, FormEvent, useEffect, useState} from 'react'
 import {signIn} from 'next-auth/client'
+import { useRouter } from 'next/router'
 
 import logo from '../assets/logo.svg'
 import Container from '../styles/pages/login'
+import LoadingModal from '../components/modals/Loading'
+import useUser from '../hooks/useUser'
+import errorAlert from '../utils/alerts/error'
 
-export default function Home() {
+export default function Login()
+{
+	const {user} = useUser()
+	const router = useRouter()
+
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+
+	const [showedError, setShowedError] = useState(false)
+	const [loading, setLoading] = useState(false)
+
+	useEffect(() =>
+	{
+		if (user.errorMessage && !showedError)
+		{
+			setShowedError(true)
+			errorAlert(user.errorMessage)
+		}
+
+		if (user.id !== 'not-logged' && router.pathname === '/login')
+			router.back()
+	}, [user])
 	
 	function handleChange(e: ChangeEvent<HTMLInputElement>)
 	{
-		if (e.target.name === 'email') setEmail(e.target.value)
-		if (e.target.name === 'password') setPassword(e.target.value)
+		if (e.target.name === 'email') 
+			setEmail(e.target.value)
+		if (e.target.name === 'password') 
+			setPassword(e.target.value)
 	}
 	
 	async function handleSubmit(e: FormEvent)
 	{
 		e.preventDefault()
+
+		setLoading(true)
 		signIn('credentials', {email, password, callbackUrl: '/'})
 	}
 	
@@ -27,36 +54,40 @@ export default function Home() {
 				<title>Entrar | Cruz Representações</title>
 			</Head>
 
-			<div className="logo">
-				<img src={logo} alt="Cruz representações"/>
+			<LoadingModal
+				isOpen={loading}
+			/>
+
+			<div className='logo'>
+				<img src={logo} alt='Cruz representações'/>
 				<h1>Cruz Representações</h1>
 			</div>
 
 			<form onSubmit={handleSubmit}>
-				<div className="fieldInput">
-					<label htmlFor="email">E-mail</label>
+				<div className='fieldInput'>
+					<label htmlFor='email'>E-mail</label>
 					<input
 						value={email}
 						onChange={handleChange}
-						type="text"
-						name="email"
-						id="email"
-						placeholder="Digite seu e-mail"
+						type='text'
+						name='email'
+						id='email'
+						placeholder='Digite seu e-mail'
 					/>
 				</div>
-				<div className="fieldInput">
-					<label htmlFor="password">Senha</label>
+				<div className='fieldInput'>
+					<label htmlFor='password'>Senha</label>
 					<input
 						value={password}
 						onChange={handleChange}
-						type="password"
-						name="password"
-						id="password"
-						placeholder="Digite sua senha"
+						type='password'
+						name='password'
+						id='password'
+						placeholder='Digite sua senha'
 					/>
 				</div>
-				<button type="submit">Entrar</button>
+				<button type='submit'>Entrar</button>
 			</form>
 		</Container>
-		)
-	}
+	)
+}
