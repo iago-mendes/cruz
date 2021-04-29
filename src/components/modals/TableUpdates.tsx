24d82aded391ab24ba8382f4144625a5dffbde50
Iago <iagobrazmendes@gmail.com>
2021-04-29 09:36:10 -0300

    define modal structure

--- src/components/modals/TableUpdates.tsx
+++ src/components/modals/TableUpdates.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
+import { FiMinus, FiPlus } from 'react-icons/fi'
 import Select from 'react-select'
 
 import { SelectOption } from '../../models'
 import api from '../../services/api'
 import Container from '../../styles/components/modals/TableUpdates'
 import { selectStyles } from '../../styles/global'
+import FormButtons from '../FormButtons'
 import NumberInput from '../NumberInput'
 import ModalContainer from './Container'
 
@@ -26,6 +28,7 @@ const TableUpdatesModal: React.FC<TableUpdatesModalProps> = ({isOpen, setIsOpen,
 {
 	const [companyTables, setCompanyTables] = useState<CompanyTable[]>([])
 	const [targetTable, setTargetTable] = useState({id: '', change: 1})
+	const [relatedTables, setRelatedTables] = useState<Array<{id: string, relation: number}>>([])
 	
 	const targetTableOptions: SelectOption[] = companyTables
 		.map(table => (
@@ -60,21 +63,46 @@ const TableUpdatesModal: React.FC<TableUpdatesModalProps> = ({isOpen, setIsOpen,
 			})
 	}, [])
 
+	function handleAddRelatedTable()
+	{
+		let tmpRelatedTables = [...relatedTables]
+		tmpRelatedTables.push({id: '', relation: 1})
+
+		setRelatedTables(tmpRelatedTables)
+	}
+
+	function handleRemoveRelatedTable(index: number)
+	{
+		let tmpRelatedTables = [...relatedTables]
+		tmpRelatedTables.splice(index, 1)
+
+		setRelatedTables(tmpRelatedTables)
+	}
+
+	function handleCancel()
+	{}
+
+	function handleSubmit()
+	{}
+
 	return (
 		<ModalContainer
 			isOpen={isOpen}
 			setIsOpen={setIsOpen}
 		>
 			<Container>
+				<h1>Atualização de tabelas</h1>
 				<div className='target'>
-					<label>Atualização de tabela</label>
+					<label>Tabela principal</label>
 					<Select
 						value={targetTableOptions.find(option => option.value === targetTable.id)}
 						options={targetTableOptions}
 						onChange={option => setTargetTable({id: option.value, change: 1})}
 						styles={selectStyles}
 					/>
-					<div className='change'>
+
+					<label>Mudança</label>
+					<div className='changeGroup'>
 						<Select
 							options={changeOptions}
 							styles={selectStyles}
@@ -87,11 +115,49 @@ const TableUpdatesModal: React.FC<TableUpdatesModalProps> = ({isOpen, setIsOpen,
 				</div>
 
 				<ul className='related'>
-					<Select
-						options={relatedTableOptions}
-						styles={selectStyles}
-					/>
+					<h2>Tabelas relacionadas</h2>
+
+					{relatedTables.map((table, index) => (
+						<div className='table' key={index} >
+							<label>Tabela</label>
+							<Select
+								options={relatedTableOptions}
+								styles={selectStyles}
+							/>
+
+							<label>Relação</label>
+							<div className='changeGroup'>
+								<Select
+									options={changeOptions}
+									styles={selectStyles}
+								/>
+								<NumberInput
+									value={targetTable.change}
+									setValue={n => setTargetTable({id: targetTable.id, change: n})}
+								/>
+							</div>
+
+							<button className='listButton remove' onClick={() => handleRemoveRelatedTable(index)}>
+								<FiMinus />
+								<span>
+									Remover representada
+								</span>
+							</button>
+						</div>
+					))}
+
+					<button className='listButton add' onClick={handleAddRelatedTable}>
+						<FiPlus />
+						<span>
+							Adicionar tabela
+						</span>
+					</button>
 				</ul>
+
+				<FormButtons
+					handleCancel={handleCancel}
+					handleSubmit={handleSubmit}
+				/>
 			</Container>
 		</ModalContainer>
 	)
