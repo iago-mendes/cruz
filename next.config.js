8ab48c878f2edbbbf6ec35009df78ac12686e8d7
Iago <iagobrazmendes@gmail.com>
2021-05-04 09:45:31 -0300

    setup pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 const withImages = require('next-images')
-module.exports = withImages(
+const withPWA = require('next-pwa')
+
+module.exports = withImages(withPWA(
 {
 	esModule: true,
 	publicRuntimeConfig:
 	{
 		apiUrl: process.env.API_URL,
 		apiKey: process.env.API_KEY
+	},
+	pwa:
+	{
+		dest: 'public'
 	}
-})
+}))
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,24 +28,47 @@ class MyDocument extends Document {
 		}
 	}
 
-  render() {
-    return (
-      <Html lang="pt">
-        <Head>
-            <meta charSet="utf-8" />
-            <link rel="icon" href="/favicon.svg" />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu&display=swap"
-                rel="stylesheet"
-            />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render()
+	{
+		const pwa =
+		{
+			name: 'Cruz',
+			description: 'Excelência em Representação Comercial!',
+		}
+
+		return (
+			<Html lang='pt'>
+				<Head>
+					<meta charSet='utf-8' />
+
+					<link rel='preconnect' href='https://fonts.gstatic.com' />
+					<link
+						href='https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu&display=swap'
+						rel='stylesheet'
+					/>
+
+					{/* PWA */}
+					
+					<meta name='application-name' content={pwa.name} />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content={pwa.name} />
+          <meta name='description' content={pwa.description} />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#84130B' />
+          
+          <link rel='apple-touch-icon' sizes='64x64' href='/favicon.svg' />
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='shortcut icon' href='/favicon.svg' type='image/svg+xml' />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
 }
 
 export default MyDocument
