fb83195a07eecab45b2de303b8efe135aaebeb5d
Iago <iagobrazmendes@gmail.com>
2021-05-14 09:19:55 -0300

    create indexeddb stores

---
+++ src/services/db/index.ts
@@ -0,0 +1,13 @@
+import Dexie from 'dexie'
+
+const db = new Dexie('cruz')
+db.version(1).stores(
+	{
+		companies: '_id, imagem, razao_social, nome_fantasia, cnpj, telefones, email, descricao_curta, descricao, site, comissao, tabelas, condicoes, produtos',
+		clients: '_id, razao_social, nome_fantasia, imagem, cnpj, insc_estadual, email, senha, vendedores, representadas, endereco, status, condicoes',
+		sellers: '_id, nome, imagem, telefones, email, senha, funcao, admin, representadas',
+		requests: '_id, cliente, vendedor, representada, produtos, data, condicao, frete, contato, digitado_por, tipo, status'
+	}
+)
+
+export default db
