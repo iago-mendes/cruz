3837db804f0216737f78b1dd8bf250eac76d4066
Iago <iagobrazmendes@gmail.com>
2021-06-04 12:01:00 -0300

    fix open pdf

--- src/services/pdf.ts
+++ src/services/pdf.ts
@@ -24,5 +24,41 @@ export function createPdf(content: any, title: string, options = {})
 		}
 	}
 
-	pdfMake.createPdf(docDefinition, options).open({})
+	function openBase64NewTab(base64Pdf: string)
+	{
+		const blob = base64toBlob(base64Pdf)
+
+		if (window.navigator && window.navigator.msSaveOrOpenBlob)
+			window.navigator.msSaveOrOpenBlob(blob, `${title}.pdf`)
+		else
+		{
+			const blobUrl = URL.createObjectURL(blob)
+			window.open(blobUrl)
+		}
+	}
+
+	function base64toBlob(base64Data: string)
+	{
+		const sliceSize = 1024
+		const byteCharacters = atob(base64Data)
+		const bytesLength = byteCharacters.length
+		const slicesCount = Math.ceil(bytesLength / sliceSize)
+		const byteArrays = new Array(slicesCount)
+
+		for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex)
+		{
+			const begin = sliceIndex * sliceSize
+			const end = Math.min(begin + sliceSize, bytesLength)
+
+			const bytes = new Array(end - begin)
+			for (let offset = begin, i = 0; offset < end; ++i, ++offset)
+				bytes[i] = byteCharacters[offset].charCodeAt(0)
+			
+			byteArrays[sliceIndex] = new Uint8Array(bytes)
+		}
+		return new Blob(byteArrays, { type: 'application/pdf' })
+	}
+
+	const doc = pdfMake.createPdf(docDefinition, options)
+	doc.getBase64((data) => openBase64NewTab(data))
 }
