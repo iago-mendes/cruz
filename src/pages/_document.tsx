2d936139b0335cdbfaeed8bc50c4252a5ce93bd1
Iago <iagobrazmendes@gmail.com>
2021-06-26 15:11:27 -0300

    style: update icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,6 +46,7 @@ class MyDocument extends Document {
 			<Html lang="pt">
 				<Head>
 					<meta charSet="utf-8" />
+					<link rel="icon" href="/favicon.svg" />
 
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
@@ -67,9 +68,9 @@ class MyDocument extends Document {
 					<meta name="mobile-web-app-capable" content="yes" />
 					<meta name="theme-color" content="#84130B" />
 
-					<link rel="apple-touch-icon" sizes="64x64" href="/favicon.svg" />
+					<link rel="apple-touch-icon" sizes="64x64" href="/icons/192px.png" />
 					<link rel="manifest" href="/manifest.json" />
-					<link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
+					<link rel="shortcut" href="/icons/192px.png" type="image/png" />
 				</Head>
 				<body>
 					<Main />
