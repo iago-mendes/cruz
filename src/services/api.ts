196d5be167e3435e891b8f5928ca533393f01fcf
Iago <iagobrazmendes@gmail.com>
2021-07-11 08:49:33 -0300

    fix: use authorization header with updated jwt

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,20 +3,25 @@ import {apiHandler} from './offline/apiHandler'
 
 export const apiUrl = String(process.env.NEXT_PUBLIC_API_URL)
 
-let token: string | undefined
-
-try {
-	token = localStorage.getItem('token')
-} catch (error) {
-	token = undefined
-}
-
 const api = axios.create({
 	baseURL: apiUrl,
 	headers: {
-		key: process.env.NEXT_PUBLIC_API_KEY,
-		authorization: `Bearer ${token}`
+		key: process.env.NEXT_PUBLIC_API_KEY
 	}
+})
+
+api.interceptors.request.use(config => {
+	let token: string | undefined
+
+	try {
+		token = localStorage.getItem('token')
+	} catch (error) {
+		token = undefined
+	}
+
+	config.headers.authorization = `Bearer ${token}`
+
+	return config
 })
 
 api.interceptors.request.use(apiHandler)
