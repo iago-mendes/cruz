6e771ebb68800012abea50b0f2f1d033ac3faad4
Iago <iagobrazmendes@gmail.com>
2021-07-13 10:58:02 -0300

    chore: organize types

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,10 +1,10 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
 import 'styled-components'
 
-import theme from './theme'
+import theme from '../styles/theme'
 
 export type Theme = typeof theme
 
 declare module 'styled-components' {
+	// eslint-disable-next-line @typescript-eslint/no-empty-interface
 	export interface DefaultTheme extends Theme {}
 }
