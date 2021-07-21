b6198c904642619a3632d59c8bfe3e7ae3a52be6
Iago <iagobrazmendes@gmail.com>
2021-07-21 17:38:01 -0300

    feat: compress images in table dropzone

--- src/components/TableDropzone/index.tsx
+++ src/components/TableDropzone/index.tsx
@@ -1,7 +1,10 @@
 import React, {useCallback, useState, useEffect} from 'react'
 import {useDropzone} from 'react-dropzone'
+import Compressor from 'compressorjs'
 
 import {Image} from '../Image'
+import errorAlert from '../../utils/alerts/error'
+import Loading from '../Loading'
 
 type Props = {
 	shownFileUrl?: string
@@ -10,6 +13,7 @@ type Props = {
 
 export function TableDropzone({shownFileUrl, onFileUploaded}: Props) {
 	const [selectedFileUrl, setSelectedFileUrl] = useState('')
+	const [loading, setLoading] = useState(false)
 
 	useEffect(() => {
 		if (shownFileUrl) setSelectedFileUrl(shownFileUrl)
@@ -18,9 +22,7 @@ export function TableDropzone({shownFileUrl, onFileUploaded}: Props) {
 	const onDrop = useCallback(
 		acceptedFiles => {
 			const file = acceptedFiles[0]
-			const fileUrl = URL.createObjectURL(file)
-			setSelectedFileUrl(fileUrl)
-			onFileUploaded(file)
+			handleUploadImage(file)
 		},
 		[onFileUploaded]
 	)
@@ -30,14 +32,55 @@ export function TableDropzone({shownFileUrl, onFileUploaded}: Props) {
 		accept: 'image/*'
 	})
 
+	async function handleUploadImage(file: File) {
+		setLoading(true)
+
+		await new Promise((resolve, reject) => {
+			new Compressor(file, {
+				quality: 0.6,
+				convertSize: 2 * 1024 * 1024, // 2mb
+				success: blobResult => {
+					const fileResult = new File([blobResult], file.name, {
+						type: 'image/png'
+					})
+					resolve(fileResult)
+				},
+				error: error => reject(error)
+			})
+		})
+			.then((fileResult: File) => {
+				if (fileResult.size > 2 * 1024 * 1024)
+					return errorAlert(
+						'O limite de upload é 2mb por arquivo.',
+						'Imagem muito grande!'
+					)
+
+				const fileUrl = URL.createObjectURL(fileResult)
+				setSelectedFileUrl(fileUrl)
+
+				onFileUploaded(fileResult)
+			})
+			.catch(() => {
+				errorAlert('Erro ao comprimir imagens.')
+			})
+
+		setLoading(false)
+	}
+
 	return (
 		<div
 			{...getRootProps()}
 			title="Selecionar imagem"
 			style={{cursor: 'pointer'}}
 		>
-			<input {...getInputProps()} accept="image/*" />
-			<Image src={selectedFileUrl} alt="Image thumbnail" />
+			{loading ? (
+				<Loading />
+			) : (
+				<>
+					<input {...getInputProps()} accept="image/*" />
+					<Image src={selectedFileUrl} alt="Image thumbnail" />
+				</>
+			)}
 		</div>
 	)
 }
