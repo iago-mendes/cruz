db377447f4d805af419988e7fcbf1a59887a623f
Iago <iagobrazmendes@gmail.com>
2021-07-27 11:00:36 -0300

    feat: handle upload products image

--- src/components/_modals/UpdateProductsImage/index.tsx
+++ src/components/_modals/UpdateProductsImage/index.tsx
@@ -1,6 +1,14 @@
+import {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import Compressor from 'compressorjs'
+import {FiUpload, FiX} from 'react-icons/fi'
+
 import {Container} from './styles'
 import FormButtons from '../../FormButtons'
 import ModalContainer from '../Container'
+import errorAlert from '../../../utils/alerts/error'
+import Loading from '../../Loading'
+import {Image} from '../../Image'
 
 type Props = {
 	isOpen: boolean
@@ -10,6 +18,64 @@ type Props = {
 }
 
 const UpdateProductsImageModal: React.FC<Props> = ({isOpen, setIsOpen}) => {
+	const [imageFiles, setImageFiles] = useState<File[]>([])
+	const [loading, setLoading] = useState(false)
+
+	const onDrop = useCallback((acceptedFiles: File[]) => {
+		handleUploadImages(acceptedFiles)
+	}, [])
+
+	const {getRootProps, getInputProps} = useDropzone({
+		onDrop,
+		accept: 'image/*'
+	})
+
+	async function handleUploadImages(files: File[]) {
+		setLoading(true)
+
+		await Promise.all(
+			files.map(async file => {
+				await new Promise((resolve, reject) => {
+					new Compressor(file, {
+						quality: 0.6,
+						convertSize: 2 * 1024 * 1024, // 2mb
+						success: blobResult => {
+							const fileResult = new File([blobResult], file.name, {
+								type: 'image/png'
+							})
+							resolve(fileResult)
+						},
+						error: error => reject(error)
+					})
+				})
+					.then((fileResult: File) => {
+						if (fileResult.size > 2 * 1024 * 1024)
+							return errorAlert(
+								'O limite de upload é 2mb por arquivo.',
+								'Imagem muito grande!'
+							)
+
+						addImage(fileResult)
+					})
+					.catch(() => {
+						errorAlert(`Erro ao comprimir ${file.name}.`)
+					})
+			})
+		)
+
+		setLoading(false)
+	}
+
+	function addImage(file: File) {
+		setImageFiles(prev => [...prev, file])
+	}
+
+	function handleRemoveImage(index: number) {
+		const tmpImageFiles = [...imageFiles]
+		tmpImageFiles.splice(index, 1)
+		setImageFiles(tmpImageFiles)
+	}
+
 	function handleCancel() {
 		setIsOpen(false)
 	}
@@ -20,6 +86,42 @@ const UpdateProductsImageModal: React.FC<Props> = ({isOpen, setIsOpen}) => {
 		<ModalContainer isOpen={isOpen} setIsOpen={setIsOpen}>
 			<Container>
 				<h1>Atualização de imagens</h1>
+
+				<div className="content">
+					<div className="dropzone" {...getRootProps()}>
+						{loading ? (
+							<Loading />
+						) : (
+							<>
+								<input {...getInputProps()} accept="image/*" name="dropzone" />
+								<p>
+									<FiUpload />
+									Selecione as imagens
+								</p>
+							</>
+						)}
+					</div>
+
+					<ul className="images">
+						{imageFiles.map((image, index) => (
+							<li key={image.name}>
+								<button
+									onClick={() => handleRemoveImage(index)}
+									title="Remover"
+								>
+									<FiX />
+								</button>
+
+								<figure>
+									<Image src={URL.createObjectURL(image)} alt={image.name} />
+								</figure>
+
+								<span>{image.name}</span>
+							</li>
+						))}
+					</ul>
+				</div>
+
 				<FormButtons handleCancel={handleCancel} handleSubmit={handleSubmit} />
 			</Container>
 		</ModalContainer>
--- src/components/_modals/UpdateProductsImage/styles.ts
+++ src/components/_modals/UpdateProductsImage/styles.ts
@@ -1,3 +1,126 @@
 import styled from 'styled-components'
 
-export const Container = styled.main``
+export const Container = styled.main`
+	padding-bottom: 5rem;
+
+	h1 {
+		width: fit-content;
+		margin: 0 auto;
+	}
+
+	.content {
+		margin: 2rem auto;
+		padding: 0 2rem;
+		max-width: 75rem;
+
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+
+		.dropzone {
+			width: 100%;
+			height: 20rem;
+
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			background-color: #fff;
+			border-radius: 1rem;
+			outline: 0;
+
+			cursor: pointer;
+			transition: 0.25s;
+
+			:hover {
+				transform: scale(1.01);
+			}
+
+			p {
+				width: calc(100% - 6rem);
+				height: calc(100% - 6rem);
+
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				gap: 0.25rem;
+
+				border: ${p => p.theme.colors.primary} 1px dashed;
+				border-radius: 1rem;
+
+				color: ${p => p.theme.colors.primary};
+				font-family: Ubuntu;
+				font-size: 1.5rem;
+				font-weight: bold;
+
+				text-align: center;
+				padding: 1rem;
+
+				svg {
+					width: 2.5rem;
+					height: 2.5rem;
+					color: ${p => p.theme.colors.primary};
+				}
+			}
+		}
+
+		ul.images {
+			display: flex;
+			flex-direction: column;
+			gap: 1rem;
+
+			li {
+				list-style: none;
+
+				display: flex;
+				align-items: center;
+				gap: 1rem;
+
+				padding: 0.5rem;
+				border-radius: 0.5rem;
+				transition: background-color 0.2s;
+
+				:hover {
+					background-color: rgba(0, 0, 0, 0.1);
+				}
+
+				button {
+					background: none;
+					border: none;
+
+					width: 3rem;
+					height: 3rem;
+					font-size: 2rem;
+					border-radius: 50%;
+
+					display: flex;
+					align-items: center;
+					justify-content: center;
+
+					transition: background-color 0.2s;
+
+					:hover {
+						background-color: ${p => p.theme.colors.buttonRed};
+					}
+				}
+
+				figure {
+					width: 10rem;
+					height: 10rem;
+
+					display: flex;
+					align-items: center;
+					justify-content: center;
+
+					img {
+						max-width: 100%;
+						max-height: 100%;
+
+						border-radius: 0.5rem;
+					}
+				}
+			}
+		}
+	}
+`
